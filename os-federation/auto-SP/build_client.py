1ad56994830484fe73f8d2ec408f5ffed1a6b2e8
minggLu <mylu@bu.edu>
2015-08-06 12:07:07 -0400

    update on automation

--- os-federation/auto-SP/build_client.py
+++ os-federation/auto-SP/build_client.py
@@ -15,10 +15,5 @@ def client_for_admin_user():
 # Used to execute all admin actions
 client = client_for_admin_user()  
 print client.users.list()
-auth = v3.Password(auth_url="http://128.52.183.216:5000/v3",
-                   user_id="aa43b03c0d3740418d7d785e504a9fcc",
-                   password="nomoresecrete",
-                   project_id="dc00b3b49b444c35aca7c174e2774b23")
-session = ksc_session.Session(auth=auth)
 
 
